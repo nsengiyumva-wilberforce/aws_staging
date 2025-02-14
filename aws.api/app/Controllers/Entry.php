65c761fc7745b94467c3d6ec67e3ca1c6ace119d
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-02-14 11:00:45 +0300

    new update

--- aws.api/app/Controllers/Entry.php
+++ aws.api/app/Controllers/Entry.php
@@ -1159,9 +1159,9 @@ class Entry extends BaseController
 
 
 		// Check if 'photo_file' field exists in the responses array at the specified index
-		if (!isset($entry['responses'][$index][0]['photo_file'])) {
+		if (!isset($entry['responses'][$index]['photo_file'])) {
 			// If not, update the document structure to include 'photo_file' in the responses array
-			$entry->responses[$index][0]->photo_file = $params['filename'];
+			$entry->responses[$index]->photo_file = $params['filename'];
 		}
 
 		// Update the document
